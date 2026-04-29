b7d7e8c9b28ce33ede9660795e02a67dd8304d91
jayant <jayantdahiya1204@gmail.com>
2026-04-29 21:14:23 +0530

    changelog-4

--- components/background/ShaderBackgroundMount.tsx
+++ components/background/ShaderBackgroundMount.tsx
@@ -3,6 +3,6 @@
 import dynamic from "next/dynamic";
 
 export const LiquidBackgroundMount = dynamic(
-  () => import("@/components/background/LiquidBackground").then((module) => module.LiquidBackground),
+  () => import("@/components/background/LiquidBackground").then((module) => module.default),
   { ssr: false },
 );
